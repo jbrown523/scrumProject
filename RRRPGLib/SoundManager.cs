822645601c8aabc9773133587509b7c98372fcb8
JalenB523 <75499194+JalenBrown523@users.noreply.github.com>
2023-05-13 11:25:16 -0500

    Add project files.

---
+++ RRRPGLib/SoundManager.cs
@@ -0,0 +1,30 @@
+﻿using RRRPGLib;
+using System.Media;
+
+namespace RRRPGLib;
+
+/// <summary>
+/// Used for playing sound effects but not currently
+/// used for playing background music
+/// </summary>
+public static class SoundManager {
+  private static readonly SoundPlayer sndPlayer;
+
+  static SoundManager() {
+    sndPlayer = new();
+  }
+
+  /// <summary>
+  /// Used to play a sound from resource manager
+  /// </summary>
+  /// <param name="ms">Use <see cref="ResourcesRef.Resources"/> and it's GetStream method to retrieve an audio stream to play</param>
+  public static void Play(UnmanagedMemoryStream ms) {
+    try {
+      sndPlayer.Stream = ms;
+      sndPlayer.Play();
+    }
+    catch {
+      // couldn't play sound, oh well
+    }
+  }
+}
---
+++ RRRPGLibTests/CharacterTests.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using RRRPGLib;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace RRRPGLib.Tests
+{
+    [TestClass()]
+    public class CharacterTests
+    {
+        [TestMethod()]
+        public void MakePlayerTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
+
+namespace RRRPGLibTests
+{
+    internal class CharacterTests
+    {
+    }
+}
