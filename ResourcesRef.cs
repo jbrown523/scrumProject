64a3c99e6daf3614ac452ff3a4a1fcb36fec89aa
VictoriaGrillo-132 <71782176+VictoriaGrillo-132@users.noreply.github.com>
2023-05-13 19:59:49 -0500

    Add files via upload

---
+++ ResourcesRef.cs
@@ -0,0 +1,13 @@
+﻿namespace RRRPGLib;
+
+/// <summary>
+/// Used to hold a reference to the resource manager
+/// so we can retrieve images and sounds
+/// </summary>
+public static class ResourcesRef {
+  /// <summary>
+  /// Reference to the <see cref="System.Resources.ResourceManager"/> 
+  /// object found in the client Windows Form project
+  /// </summary>
+  public static System.Resources.ResourceManager Resources;
+}
